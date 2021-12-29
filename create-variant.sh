50484a295c7fc414928622a684e61be9ef753d91
William Goodall <wgoodall01@gmail.com>
2021-12-28 21:38:09 -0500

    Add create-variant.sh

---
+++ create-variant.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+shopt -s inherit_errexit
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+if [[ "$#" != 1 ]]; then
+	echo "Usage: $0 VARIANT_NAME"
+	exit 2
+fi
+
+name="$1"
+var_dir="$DIR/variants/$name"
+
+mkdir -p "$var_dir"
+
+cp "$DIR"/{resume.tex,setup.tex,Makefile} "$var_dir"
+
+echo "Created variant '$name'"
